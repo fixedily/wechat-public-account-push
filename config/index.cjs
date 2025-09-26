a7aeefb8d20468c860bdbae00e9ef31effda0487
fixedily <fixedily@gmail.com>
2025-09-26 22:52:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,6 +44,14 @@ const USER_CONFIG = {
       //   { keyword: 'marry_day', date: '2022-09-09' },
       // ],
     },
+    {
+      // 想要发送的人的名字
+      name: '哥们啊',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oocL-17TyDcglLvLfyQzpoahDBbI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'gUf0cOTwvmX8vyhLz-WMWbLX3j_odl7DaXsxegQXYA8',
+    },
   ],
 
   TIAN_API: {
